148cfdd2088ee4c0c0e72d09253c3b0ad24e504b
AQW <alice@valour.me>
2021-09-24 10:55:22 -0700

    proofread latest jobs

--- resume/jobs.js
+++ resume/jobs.js
@@ -141,9 +141,9 @@
 		"start": "2013-07-15",
 		"end": "2014-10-31",
 		"details": [
-			"Research and develop a new visual ticket schema, using HTML5 canvas and video, and CSS3.",
-			"Implemented all of the design specification and frontend interactivity for the web application our customers use to buy tickets.",
-			"Developed an integration tool, which connected our Phabricator and Bitbucket workflow, to track tasks and release cycles."
+			"Research and develop a new visual ticket schema using HTML5 canvas and video and CSS3.",
+			"Implemented design specifications for our web application that customers users to buy their transit tickets.",
+			"Developed an integration tool that connected our Phabricator and Bitbucket workflows to track tasks and release cycles."
 		],
 		"tags": ["Java", "play framework", "Javascript", "HTML5", "<canvas>", "MySQL"]
 	},
@@ -153,9 +153,9 @@
 		"start": "2014-11-05",
 		"end": "2016-04-01",
 		"details": [
-			"Created an end-to-end solution for generation user metrics images per ride",
-			"Developed features for frontend Angular webapp, internal CMS and API (Python)",
-			"Investigated other spin cycles and botique fitness studios in the NYC"
+			"Created an end-to-end solution for generating metrics images per ride so that users can share on social media.",
+			"Developed features for home page and web application in Angular, and internal CMS, and API (Python)",
+			"Investigated other spin cycles and boutique fitness studios in NYC."
 		],
 		"tags": ["python", "PostgreSQL", "Javascript", "Angular.js"]
 	},
@@ -165,7 +165,8 @@
 		"start": "2016-04-20",
 		"end": "2016-10-31",
 		"details": [
-			"Created functional prototype for the new Mobile User Interface project"
+			"Created a functional prototype for the new mobile web player using Framer.",
+			"Investigated and fixed device-specific issues the Android SDK introduced."
 		],
 		"tags": ["Java", "Android SDK", "Framer.js"]
 	},
@@ -177,7 +178,7 @@
 		"details": [
 			"Employed by Techolutions, contracted out to J.Crew",
 			"Work on A/B tested features and improvements to their site to drive up conversion rates",
-			"Create proof of concepts to help design team define specifications an immersive product detail pages.",
+			"Create a proof of concepts to help the designer define specifications for immersive product detail pages.",
 			"Migrate their BackboneJs website to ReactJS"
 		],
 		"tags": ["ReactJs", "ES6"]
@@ -188,15 +189,15 @@
 		"start": "2018-07-01",
 		"end": "2020-07-01",
 		"details": [
-			"Identified problems our users face, and researched approaches to solve them.",
+			"Identified problems our users faced by providing support while they tried to build APIs in Dark's editor.",
 			"Found low effort/high impact changes that would improve product usability",
-			"Designed the visual treatment and interaction flow for features.",
+			"Designed features users will need to write code within the Dark editor.",
 			"Scoped feature specifications to meet project timeline and engineering resources.",
-			"Wrote detailed style guidelines and state charts for a feature, so engineers know exactly what is necessary to implement.",
-			"Supported engineers implementing the a feature, helped them with software achitecture and CSS, or changed the design so it may be easier for them to implement within timeframe.",
-			"Reduced UX and visual inconsistencies, and coalesce workflows.",
-			"Implemented solutions and features in OCaml and ReasonML, when engineering resource is low.",
-			"Designed graphical assets our company needs. ie: icons, logo, mascot, print material."
+			"Wrote detailed style guidelines and state charts for a feature, so engineers know what is necessary to implement.",
+			"Supported engineers implementing a feature, helped them with software architecture and CSS or made the designs easier to implement within the timeframe.",
+			"Reduced user experience and visual inconsistencies users experience in the editor.",
+			"Implemented solutions and features in OCaml and ReasonML whenever engineering resource is low.",
+			"Designed graphical assets for company needs. I.e., icons, logo, mascot, print material."
 		]
 	},
 	{
@@ -205,7 +206,7 @@
 		"start": "2020-08-01",
 		"end": "2020-12-31",
 		"details": [
-			"Designed features to help our client's access their order transactions & tracking.",
+			"Designed features to help our clients access their order transactions & tracking.",
 			"Implemented features in React, EmotionJs, and Typescript.",
 			"Coordinated with Product team and backend teams to design, build, and ship."
 		]
@@ -216,8 +217,8 @@
 		"start": "2021-01-04",
 		"end": "NOW",
 		"details": [
-			"Created a design system for node-editor's GUI.",
-			"Designed visual representation of programming concepts, such that they be easily understood for non-programmers.",
+			"Created a design system for our node-editor.",
+			"Designed visual representation of programming concepts so non-programmers can easily understand them.",
 			"Implemented features in Typescript using <canvas> Context2d and Svelte."
 		]
 	}
